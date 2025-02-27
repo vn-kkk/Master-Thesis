81844e2e5d678e2d38bbb045188d0dcdb4323d69
vn-kkk <vnkay24@gmail.com>
2025-02-27 17:49:42 +0100

    More organized codes.

--- bilby workshop/Cluster run 1/cluster_result/submit/13-02-fast_tutorial_data0_0_analysis_H1L1_par0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source bilby
-
-python /cluster/users/venkatek/.conda/envs/bilby/bin/bilby_pipe_analysis outdir_13.02.fast_tutorial/13.02.fast_tutorial_config_complete.ini --outdir outdir_13.02.fast_tutorial --detectors H1 --detectors L1 --label 13-02-fast_tutorial_data0_0_analysis_H1L1_par0 --data-dump-file outdir_13.02.fast_tutorial/data/13-02-fast_tutorial_data0_0_generation_data_dump.pickle --sampler dynesty
-
